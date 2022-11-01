098f7d6a014f5310f05611dfef303421422a43b1
Meiiosei <89043339+Meiiosei@users.noreply.github.com>
2022-11-01 11:54:45 +0100

    Ajout du fichier DM

---
+++ DM.py
@@ -0,0 +1,33 @@
+#   ಠ   #
+
+
+#Variables 
+n = int(input('Veuillez rentrer la valeur de n : '))
+x = 0.7
+reponse = "O"
+
+#Programme
+
+while reponse == "O" :
+    
+    while n < 1 :
+        print ("Vous devez entrer un nombre entier strictement positif !")
+        n = int(input('Veuillez rentrer la valeur de n : '))
+
+            
+    for i in range (n) : 
+                
+        x = 0.1*i + 0.7
+        print("-----------------------------------\n"
+            "La probabilité de résussite du service au service n°" , i + 1 , "sera de :" , ( round (x, 2) ) , "\n"
+            "-----------------------------------")
+            
+    reponse = input('Voulez-vous recommencez (O)ui ou (N)on ? : ')
+    if reponse == "O" :
+        n = int(input('Veuillez rentrer la valeur de n : '))
+    else :
+        break
+
+
+
+
