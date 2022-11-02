18a630f62ddbb1b7f6c2282e33ab50577b4567ce
Meiiosei <89043339+Meiiosei@users.noreply.github.com>
2022-11-02 19:46:01 +0100

    dm

--- DM.py
+++ DM.py
@@ -18,7 +18,8 @@ while reponse == "O" :
     for i in range (n) : 
                 
         x = 0.1*i + 0.7
-        print("-----------------------------------\n"
+        
+    print("-----------------------------------\n"
             "La probabilité de résussite du service au service n°" , i + 1 , "sera de :" , ( round (x, 2) ) , "\n"
             "-----------------------------------")
             
