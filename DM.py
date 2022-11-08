0d7c6184648b2a993472512855a2cbdeeab749b2
Meiiosei <89043339+Meiiosei@users.noreply.github.com>
2022-11-08 09:35:57 +0100

    Ajustement de l'énoncé

--- DM.py
+++ DM.py
@@ -1,14 +1,10 @@
-#   ಠ   #
-
-    #Constantes
-    
 reponse = "O"
 
 
     #Variables 
     
 n = int(input('Veuillez rentrer la valeur de n : '))
-x = 0.7
+x = 0
 
 
     #Programme
@@ -22,10 +18,10 @@ while reponse == "O" :
             
     for i in range (n) : 
                 
-        x = 0.1*i + 0.7
+        x = 0.1*x + 0.7
         
     print("-----------------------------------\n"
-            "La probabilité de résussite du service au service n°" , i + 1 , "sera de :" , ( round (x, 2) ) , "\n"
+            "La probabilité de résussite du service au service n°" , i + 1 , "sera de :" ,(round(x, n)) , "\n"
             "-----------------------------------")
             
     reponse = input('Voulez-vous recommencez (O)ui ou (N)on ? : ')
@@ -33,6 +29,7 @@ while reponse == "O" :
         n = int(input('Veuillez rentrer la valeur de n : '))
     else :
         break
+
 
 
 
